95d242c280c23ffada757c8a85cfd3effe2d4e6a
abue.ammar <abue.ammar@digigate360.com>
2023-12-14 15:50:20 +0600

    next ui configured

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,14 @@
+"use client";
+import { Button } from "@nextui-org/react";
 import Footer from "./_components/footer";
 
 const HomePage = () => {
   return (
     <div className="min-h-full flex flex-col dark:bg-[#1F1F1F]">
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
-        body
+        <div>
+          <Button>Click me</Button>
+        </div>
       </div>
       <Footer />
     </div>
