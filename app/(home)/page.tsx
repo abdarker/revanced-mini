3f65c08535c00f7a70ee5e3849fd970c5b672aa4
abue.ammar <abue.ammar@digigate360.com>
2023-12-18 14:55:36 +0600

    major ui changes and font changed

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,13 +6,13 @@ export default async function Page() {
     "https://api.github.com/repos/revanced-apks/build-apps/releases/latest"
   );
   const data: any = await res.json();
-  console.log("My Log: ", data);
+
   return (
     <div className="min-h-full flex flex-col  ">
       <div className="container mx-auto">
         <div className="flex flex-col gap-y-6 flex-1 px-6 pb-6 ">
           {data?.assets?.reverse().map((item: any, index: number) => (
-            <InfoCard key={index} data={item} />
+            <InfoCard key={index} data={item} body={data}/>
           ))}
         </div>
       </div>
--- components/card.tsx
+++ components/card.tsx
@@ -1,4 +1,4 @@
-import { getLogoUrl, toTitleCase } from "@/lib/util";
+import { getLogoUrl } from "@/lib/util";
 import {
   Card,
   CardHeader,
@@ -7,6 +7,7 @@ import {
   Divider,
   Link,
   Image,
+  Button,
 } from "@nextui-org/react";
 
 interface PageData {
@@ -44,21 +45,24 @@ interface PageData {
   browser_download_url: string;
 }
 
+interface Body {
+  body: string;
+  html_url: string;
+  published_at: string;
+}
+
 interface CardProps {
   data: PageData;
+  body: Body;
 }
-const InfoCard: React.FC<CardProps> = ({ data }) => {
+const InfoCard: React.FC<CardProps> = ({ data, body }) => {
   const extractedNames: string[] | undefined = data?.name.split("-");
-  const appName: string = extractedNames
-    ? toTitleCase(extractedNames[0] + " " + extractedNames[1])
-    : "";
-  const appVersion: string = extractedNames ? extractedNames[2] : "";
   const iconType: string = extractedNames ? extractedNames[0] : "";
   const iconUrl: string = getLogoUrl(iconType);
 
   return (
     <Card className="">
-      <CardHeader className="flex gap-3">
+      <CardHeader className="flex gap-3 p-2">
         <Image
           alt={iconType}
           height={40}
@@ -68,23 +72,70 @@ const InfoCard: React.FC<CardProps> = ({ data }) => {
         />
         <div className="flex flex-col flex-1">
           <p className="text-base">{data?.name} </p>
-          <p className="text-sm text-default-400">
-            {Math.round(data?.size / 1024 / 1024)} MB
-          </p>
+          <div className="flex items-center space-x-2  h-4 text-sm text-danger pt-1">
+            <div>{Math.round(data?.size / 1024 / 1024)} MB</div>
+            <Divider orientation="vertical" />
+            <div>{data?.download_count} Downloads</div>
+          </div>
         </div>
       </CardHeader>
       <Divider />
-      <CardBody>
-        <p>Make beautiful websites regardless of your design experience.</p>
+      <CardBody className="">
+        {(iconType == "youtube" || "music") && (
+          <span className="text-sm mb-0.5">
+            Install{" "}
+            <Link
+              className="text-sm"
+              href={
+                "https://github.com/TeamVanced/VancedMicroG/releases/download/v0.2.24.220220-220220001/microg.apk"
+              }
+            >
+              Vanced Microg
+            </Link>{" "}
+            for non-root YouTube and YT Music.
+          </span>
+        )}
+        <span className="text-sm mb-2">
+          See{" "}
+          <Link
+            className="text-sm"
+            href={"https://j-hc.github.io/rvmm-config-gen"}
+          >
+            the list
+          </Link>{" "}
+          of patches.
+        </span>
+        <p className="text-default-400 text-xs font-light">
+          Release Date: {new Date(body?.published_at).toDateString()}
+        </p>
       </CardBody>
       <Divider />
-      <CardFooter>
+      <CardFooter className="p-2 flex justify-between">
+        <Link isExternal href={body?.html_url} className="text-sm">
+          View Source
+        </Link>
         <Link
-          isExternal
-          showAnchorIcon
-          href="https://github.com/nextui-org/nextui"
+          isBlock
+          color="success"
+          download={true}
+          className=" rounded-xl bg-success/10 text-success text-sm font-sans"
+          href={data?.browser_download_url}
         >
-          Visit source code on GitHub.
+          Download
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5 "
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17 13l-5 5m0 0l-5-5m5 5V6"
+            />
+          </svg>
         </Link>
       </CardFooter>
     </Card>
